2ef3a5c7d2b1dbe8d1cabec6f912f36611aee2c3
TheDapperDemon <83481628+TheDapperDemon@users.noreply.github.com>
2022-03-15 11:20:50 -0600

    fixed minor bugs

---
+++ Game.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack
+{
+    public abstract class Game // abstract classes are here to lock our class because we will never make an instance of the class meaning it will never be made an object
+    {
+        private List<Player> _players = new List<Player>(); // to make sure list is never null we make this list Private
+        private Dictionary<Player, int> _bets = new Dictionary<Player, int>(); // we will make a private dictionary the key is Player and the value is the amount the user bet
+
+        public List<Player> Players { get { return _players; } set { _players = value; } } // this is the same as the get set, but now we are returning an empty list and not a null, this will stop it from throwing errors
+        public string Name { get; set; } // metho for the name of the game we will play
+
+        public Dictionary<Player, int> Bets { get { return _bets; } set { _bets = value; } } // we then make this here because we need to do this when making a private class
+
+        public abstract void Play(); // abstract method contain no implementation and can only be made inside a abstract class, these state that any class
+        // inheriting the class Game must also have this method
+        public virtual void ListPlayers()
+        // virtual means that this method gets inhertited by an inheriting class but has the ability to override it, it can be implemented but can be overwritten
+        {
+            foreach (Player player in Players) //loop through players and display their names on screen
+            {
+                Console.WriteLine(player);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack
+{
+    public class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Grand Hotel and Casino. Let's start by entering your name.");
+            string playerName = Console.ReadLine();
+            Console.WriteLine("And how much Money did your bring today?");
+            int bank = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hello, {0}. Would you like to join a Black Jack Game right now?");
+            string answer = Console.ReadLine().ToLower();
+
+            if(answer =="yes" || answer =="yeah" || answer =="ya" || answer =="y")
+            {
+                Player player = new Player(playerName, bank);
+                Game game = new BlackJackGame();
+                game += player;
+                player.isActivelyPlaying = true;
+                while (player.isActivelyPlaying && player.Balance > 0)
+                {
+                    game.Play();
+                }
+                game -= player;
+                Console.WriteLine("Thank you for playing BlackJack!");
+            }
+
+            Console.WriteLine("Feel free to look around the Casino! Bye for now.");
+            Console.ReadLine();
+        }
+    }
+}
